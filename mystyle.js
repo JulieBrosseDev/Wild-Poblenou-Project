6293ee324883b2c7408bcf1113592fb6d5cd918d
Julie Brosse <privatejuliedev@gmail.com>
2020-01-21 11:59:06 +0100

    background of welcome page changed

--- mystyle.js
+++ mystyle.js
@@ -1,2 +1,9 @@
-// Animations init
-new WOW().init();
+
+let isWellDressed = true;
+let age = 18;
+
+if (isWellDressed === true && age > 18 ) {
+    console.log ("welcome to the club")
+    }   else {
+        console.log ("sorry, next time!")
+    };
