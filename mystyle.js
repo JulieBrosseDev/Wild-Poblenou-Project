ff5abd04e1dc253f8b2ec2f4ae0458b49e40b926
Julie Brosse <privatejuliedev@gmail.com>
2020-01-22 10:18:27 +0100

    before sticking position of the small screen

---
+++ mystyle.js
@@ -0,0 +1,5 @@
+let classCountries = ["Barcelona", "Almeria", "Argentina", "Germany"];
+
+for(let i=0; i < classCountries.length; i += 1) {
+    console.log("We have someone from " + classCountries[i])
+}
