71da85cdbddb19f272a2cf48f5aadb602d8c2a95
Julie Brosse <privatejuliedev@gmail.com>
2020-01-22 17:31:16 +0100

    display none wooooorks. lets try to iplement the footer

--- mystyle.js
+++
@@ -1,5 +0,0 @@
-let classCountries = ["Barcelona", "Almeria", "Argentina", "Germany"];
-
-for(let i=0; i < classCountries.length; i += 1) {
-    console.log("We have someone from " + classCountries[i])
-}
