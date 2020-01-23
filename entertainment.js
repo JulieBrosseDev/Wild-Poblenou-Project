a9e6dd8d04ef540069dad314dd67a79901a14245
Rut <rut.abello@gmail.com>
2020-01-23 23:16:45 +0100

    Changed 'babies' image. Solved nadbar over music menu problem. Changed the setTimeout to 200 to fix bug. Smooth scroll in all html.

--- entertainment.js
+++ entertainment.js
@@ -1,23 +1,23 @@
 document.getElementById('music-tab').addEventListener('click', function(){
     setTimeout(function(){
-        window.location.href ='#test';
+        window.location.href ='#music';
     }, 200);
 });
 
 document.getElementById('sports-tab').addEventListener('click', function(){
     setTimeout(function(){
         window.location.href ='#sports';
-    }, 100);
+    }, 200);
 });
 
 document.getElementById('restaurants-tab').addEventListener('click', function(){
     setTimeout(function(){
         window.location.href ='#restaurants';
-    }, 100);
+    }, 200);
 });
 
 document.getElementById('arts-tab').addEventListener('click', function(){
     setTimeout(function(){
         window.location.href ='#arts';
-    }, 100);
+    }, 200);
 });
